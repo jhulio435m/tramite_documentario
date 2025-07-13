bba8889c416e01c39098b5cf80bccb9ed2a8ea90
Jhulio Alessandro Morán De La Cruz <jhulio435@gmail.com>
2025-07-13 16:50:18 -0500

    Revert "Codex"

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->firstName(),
-            'last_name' => fake()->lastName(),
+            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'dni' => str_pad((string) fake()->unique()->numberBetween(0, 99999999), 8, '0', STR_PAD_LEFT),
-            'role_id' => 1,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
