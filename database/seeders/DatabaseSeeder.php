97622ff203c3125fba6115358780cc850d884724
jhulio435m <76138183+jhulio435m@users.noreply.github.com>
2025-07-14 02:55:57 -0500

    Volviendo manualmente al estado de c7d4c3e

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test',
-            'last_name' => 'User',
-            'dni' => '12345678',
+            'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
     }
