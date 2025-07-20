1bd9763afc53a5d6b99c6abeb76b5381f6302987
jhulio435m <76138183+jhulio435m@users.noreply.github.com>
2025-07-20 16:53:32 -0500

    actualización de filter

--- database/migrations/2025_07_21_043113_create_expedientes_table.php
+++ database/migrations/2025_07_21_043113_create_expedientes_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('dni', 8)->nullable();
             $table->year('year')->nullable()->index();
-            $table->foreignId('month_id')->nullable()->constrained('months')->index();
-            $table->foreignId('faculty_id')->nullable()->constrained('facultades')->index();
-            $table->foreignId('tramite_type_id')->nullable()->constrained('tramite_types')->index();
-            $table->foreignId('status_id')->nullable()->constrained('statuses')->index();
+            $table->foreignId('month_id')->nullable()->constrained('months')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('faculty_id')->nullable()->constrained('facultades')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('tramite_type_id')->nullable()->constrained('tramite_types')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('status_id')->nullable()->constrained('statuses')->nullOnDelete()->cascadeOnUpdate();
             $table->string('sumilla');
             $table->text('observaciones')->nullable();
             $table->timestamps();
